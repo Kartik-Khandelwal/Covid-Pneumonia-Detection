723854c8d962234b3a2c8065941c72b0c2c159f8
Kartik Khandelwal <69040018+Kartik-Khandelwal@users.noreply.github.com>
2022-01-16 04:45:13 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+echo "[theme]
+primaryColor = ‘#84a3a7’
+backgroundColor = ‘#EFEDE8’
+secondaryBackgroundColor = ‘#fafafa’
+textColor= ‘#424242’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
